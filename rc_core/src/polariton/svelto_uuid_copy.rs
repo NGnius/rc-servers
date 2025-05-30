1a7400d3d9a124366a2520d6396369fa29d48b08
NG (Graham) <ngniusness@gmail.com>
2025-05-30 17:29:56 -0400

    Force encryption for all server op responses

--- rc_core/src/polariton/svelto_uuid_copy.rs
+++ rc_core/src/polariton/svelto_uuid_copy.rs
@@ -7,12 +7,12 @@ impl OpIdCopy {
 }
 
 impl <C: Clone + Send + Sync + 'static> OperationModifier<C> for OpIdCopy {
-    fn after(&self, req: &mut polariton::operation::OperationRequest<C>, resp: &mut polariton::operation::OperationResponse<C>, _flags: &mut u8) {
+    fn after(&self, req: &mut polariton::operation::OperationRequest<C>, resp: &mut polariton::operation::OperationResponse<C>, flags: &mut u8) {
         if resp.params.get(&Self::SERVICE_MAPPING_KEY).is_none() {
             if let Some(svelto_service_id) = req.params.get(&Self::SERVICE_MAPPING_KEY) {
                 resp.params.insert(Self::SERVICE_MAPPING_KEY, svelto_service_id.to_owned());
             }
         }
-
+        *flags |= 0x80;
     }
 }
