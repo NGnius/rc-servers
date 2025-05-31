ec9e98c59c365b0fb1db7befea8d034a860c7fdd
NG (Graham) <ngniusness@gmail.com>
2025-05-31 11:58:05 -0400

    Fix error on end of singleplayer match

--- rc_singleplayer_room/src/operations/mod.rs
+++ rc_singleplayer_room/src/operations/mod.rs
@@ -11,4 +11,5 @@ pub fn handler(init_ctx: &crate::InitConfig) -> OperationsHandler<crate::UserTy>
         .add(eac::EacChallengeIgnorer)
         .add(load_ai_robots::tdm_machines_provider(&init_ctx.factory, init_ctx.parsers.weapon_order()))
         //.add(polariton_server::operations::Ack::<33, _>::default()) // get user clan info (this is equivalent to not being in a clan)
+        .add(polariton_server::operations::Ack::<2, _>::default()) // Save singleplayer result (parameter-less response)
 }
