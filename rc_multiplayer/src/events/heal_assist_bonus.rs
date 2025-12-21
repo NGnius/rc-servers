ba21f77743d658b24add498bd11365614ec3b5e5
NG (Graham) <ngniusness@gmail.com>
2025-12-20 22:03:53 -0500

    Remove heal assist log info message, rename

--- rc_multiplayer/src/events/heal_assist_bonus.rs
+++ rc_multiplayer/src/events/heal_assist_bonus.rs
@@ -1,12 +1,12 @@
-pub struct HealBonus {
+pub struct HealAssistBonus {
     msg_router: tokio::sync::mpsc::Sender<crate::matches::GameMessage>,
 }
 
-pub(super) fn handler(init_ctx: &crate::InitConfig) -> crate::handlers::SimpleRlnl<rlnl::events::ingame::HealAssistBonus, HealBonus> {
-    crate::handlers::SimpleRlnl::new(HealBonus::new(init_ctx))
+pub(super) fn handler(init_ctx: &crate::InitConfig) -> crate::handlers::SimpleRlnl<rlnl::events::ingame::HealAssistBonus, HealAssistBonus> {
+    crate::handlers::SimpleRlnl::new(HealAssistBonus::new(init_ctx))
 }
 
-impl HealBonus {
+impl HealAssistBonus {
     fn new(init_ctx: &crate::InitConfig) -> Self {
         Self {
             msg_router: init_ctx.matches_chann.clone(),
@@ -15,13 +15,13 @@ impl HealBonus {
 }
 
 #[async_trait::async_trait]
-impl crate::handlers::RlnlEventCodeHandler for HealBonus {
+impl crate::handlers::RlnlEventCodeHandler for HealAssistBonus {
     type In = rlnl::events::ingame::HealAssistBonus;
     const CODE: rlnl::event_code::NetworkEvent = rlnl::event_code::NetworkEvent::HeallingAssistBonusRequest;
 
     async fn handle(&self, data: Self::In, _peer: &std::sync::Arc<literustlib_server::Connection<crate::PacketData>>, user: &crate::UserData, _sender: &std::sync::Arc<literustlib_server::DataSender<crate::PacketData>>) {
         if let Some(user_info) = user.user().await {
-            log::info!("Heal assist for player {}", data.healing_player_id);
+            //log::info!("Heal assist for player {}", data.healing_player_id);
             super::log_channel_send_failure(self.msg_router.send(crate::matches::GameMessage::HealAssistBonus {
                 user_id: user_info.user_id(),
                 healer: data.healing_player_id,
