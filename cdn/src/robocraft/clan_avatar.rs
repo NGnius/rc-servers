8c2a90cea7f7333e4b630d1b0ca39a98d69b5ad4
NG (Graham) <ngniusness@gmail.com>
2026-04-06 10:22:10 -0400

    Fix clan avatars not displaying in some cases

--- cdn/src/robocraft/clan_avatar.rs
+++ cdn/src/robocraft/clan_avatar.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, web::{Data, Path, Bytes}, Responder};
 
 #[get("/clanavatar/Live/{name}")]
 pub async fn get(cli: Data<crate::cli::CliArgs>, name: Path<String>) -> impl Responder {
-    let path = std::path::PathBuf::from(&cli.data_robocraft).join("clanavatar").join(format!("{}.jpg", *name));
+    let path = std::path::PathBuf::from(&cli.data_robocraft).join("clanavatar").join(format!("{}.jpg", name.to_lowercase()));
     log::debug!("RC asset at {} (exists? {})", path.display(), path.exists());
     if path.exists() {
         actix_files::NamedFile::open_async(path).await
@@ -15,7 +15,7 @@ pub async fn get(cli: Data<crate::cli::CliArgs>, name: Path<String>) -> impl Res
 #[post("/clanavatar/Live/{name}")]
 pub async fn post(cli: Data<crate::cli::CliArgs>, auth: Data<crate::robocraft::IntercomAuth>, name: Path<String>, body: Bytes, req: actix_web::HttpRequest) -> Result<actix_web::HttpResponse, super::IntercomOpError> {
     auth.validate(&req, &name)?;
-    let path = std::path::PathBuf::from(&cli.data_robocraft).join("clanavatar").join(format!("{}.jpg", *name));
+    let path = std::path::PathBuf::from(&cli.data_robocraft).join("clanavatar").join(format!("{}.jpg", name.to_lowercase()));
     log::debug!("Saving clanavatar for {} to {}: {}B", name, path.display(), body.len());
     std::fs::write(path, &body).map_err(super::IntercomOpError::Io)?;
     Ok(actix_web::HttpResponse::NoContent().finish())
